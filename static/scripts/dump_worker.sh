c0c574cf63462c7ef73f6be3d1573fb759a140cc
Tomás Restrepo <nilksermot@gmail.com>
2014-04-11 15:43:51 +0200

    again, MySQL scripts created

--- static/scripts/dump_worker.sh
+++
@@ -1,1 +0,0 @@
-mysqldump --user='clminer' -p'cloudminer2014' cloudminer worker > ./dumps/worker_bak.sql --compact --no-create-info --complete_insert --skip-quote-names
--- static/scripts/dump_worker_stats.sh
+++
@@ -1,1 +0,0 @@
-mysqldump --user='clminer' -p'cloudminer2014' cloudminer worker_stats > ./dumps/worker_stats_bak.sql --compact --no-create-info --complete_insert --skip-quote-names

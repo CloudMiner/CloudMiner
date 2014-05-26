56739b5e0e6362c20728dc0af7192a380ef8e2cb
juan <juan@juan>
2014-05-26 19:18:38 +0200

    Update and fixed worker_stats controller

--- controllers/worker_stats.py
+++ controllers/worker_stats.py
@@ -1,4 +1,10 @@
 def index():
     #workers = db().select(db.worker.ALL)
-    grid = SQLFORM.smartgrid(db.worker_stats, create=False, editable=False, deletable=True)
+    grid = SQLFORM.grid(db.worker_stats,
+                             fields = [db.worker_stats.worker_id,db.worker_stats.hash_rate,db.worker_stats.hash_avg,db.worker_stats.timestamp],
+                             headers = {'worker_stats.worker_id': 'Worker name',
+                                       'worker_stats.hash_rate': 'Hash rate',
+                                       'worker_stats.hash_avg': 'Hash average',
+                                       'worker_stats.timestamp': 'Time stamp' },
+                             searchable=False, create=False, editable=False, deletable=False, details=False, csv=False)
     return locals()

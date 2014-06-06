04ccecce760efeceefc16b4fadb5568c866e311e
Tomás Restrepo <nilksermot@gmail.com>
2014-06-06 23:04:25 +0200

    temporal controller over total hashes

--- controllers/default.py
+++ controllers/default.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
-
+from datetime import *
 from time import sleep
 import logging
 #import pymysql
 import random
 import re
 import sys
+now = datetime.strptime(datetime.now().strftime('%Y/%m/%d %H:%M:%S'), '%Y/%m/%d %H:%M:%S')
 
 def index():
     """
@@ -26,6 +19,20 @@ def index():
     """
     response.flash = T("Welcome to web2py!")
     return dict(message=T('Hello World'))
+
+def total_hashes():
+    rows = db.executesql('SELECT c.name, SUM(ws1.hash_avg) hash_sum FROM currency c, miner m, worker w, worker_stats ws1,	(SELECT worker_id, MAX(hash_count) hash_count FROM worker_stats GROUP BY worker_id) ws2 WHERE (w.time_stop IS NULL OR w.time_stop>NOW()) AND ws1.worker_id = w.id AND m.id = w.miner_id AND c.id = m.currency_id AND ws1.hash_count = ws2.hash_count AND ws1.worker_id = ws2.worker_id AND ws1.hash_avg != 0 GROUP BY c.name', as_dict=True) #, colnames=[db.currency.name, db.worker_stats.hash_avg]
+    total_bitcoin = None
+    total_litecoin= None
+    print rows
+    for row in rows:
+        print row
+        if row['name'] == 'Bitcoin':
+            total_bitcoin = row['hash_sum']
+        elif row['name'] == 'LiteCoin':
+            total_litecoin = row['hash_sum']
+        #print row['name'] , ': ' , row['hash_sum']
+    return locals
 
 
 def user():
@@ -85,3 +92,38 @@ def workers_active():
     #workers = db().select(db.worker.ALL)
     grid = SQLFORM.smartgrid(db.worker, create=False, editable=False, deletable=True)
     return locals()
+
+def donut():
+    online_query = db(db.machine.alive=='T')
+    online = online_query.count()
+    if not online:
+        online = 0
+    rows = db((db.worker.time_stop == None) | (db.worker.time_stop > now)).select(db.worker.ALL, distinct=True)
+    mining = 0
+    for row in rows:
+        mining += 1
+    online -= mining
+    offline = db(db.machine.alive=='F').count()
+    if not offline:
+        offline = 0
+    total = db(db.machine).count()
+    other = total - online - offline - mining
+    
+    tlist = []
+    elem1 = {}
+    elem1['label'] = 'online-idle'
+    elem1['value'] = online
+    tlist.append(elem1)
+    elem2 = {}
+    elem2['label'] = 'offline'
+    elem2['value'] = offline
+    tlist.append(elem2)
+    elem3 = {}
+    elem3['label'] = 'online-mining'
+    elem3['value'] = mining
+    tlist.append(elem3)
+    elem4 = {}
+    elem4['label'] = 'other'
+    elem4['value'] = other
+    tlist.append(elem4)
+    return dict(rows = tlist)
